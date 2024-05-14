eac56eca83a949470442106b9b35c094f50aee99
VadimLitau <vadimlitau@yandex.ru>
2024-05-14 15:23:15 +0500

    сделал слайдер

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import header from "./modules/header";
 import download from "./modules/downloadn";
 import enterPhone from "./modules/enterPhone";
+import test from "./modules/test";
 
 header();
 download();
 enterPhone();
+test();
---
+++ src/js/modules/test.js
@@ -0,0 +1,20 @@
+// Default theme
+import "@splidejs/splide/css";
+
+// // or other themes
+// import "@splidejs/splide/css/skyblue";
+// import "@splidejs/splide/css/sea-green";
+
+// or only core styles
+import "@splidejs/splide/css/core";
+import Splide from "@splidejs/splide";
+
+new Splide(".splide", {
+	rewindByDrag: true,
+	// autoWidth: true,
+	// autoHeight: true,
+	arrows: false,
+	classes: {
+		pagination: "splide__pagination your-class-pagination",
+	},
+}).mount();
