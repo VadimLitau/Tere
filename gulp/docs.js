89c36f516ce394f371b8f0945cc90baa4a9daaa5
VadimLitau <vadimlitau@yandex.ru>
2024-05-22 16:22:47 +0500

    docs fix

--- gulp/docs.js
+++ gulp/docs.js
@@ -25,134 +25,125 @@ const typograf = require("gulp-typograf");
 
 const extReplace = require("gulp-ext-replace");
 function plumberNotify(title) {
-  return {
-    errorrHandler: notify.onError({
-      title: title,
-      message: "Error <%= error.message %>",
-      sound: false,
-    }),
-  };
+	return {
+		errorrHandler: notify.onError({
+			title: title,
+			message: "Error <%= error.message %>",
+			sound: false,
+		}),
+	};
 }
 
 const fileIncludeSettings = {
-  prefix: "@@",
-  basepath: "@file",
+	prefix: "@@",
+	basepath: "@file",
 };
 
 const fileStartServerSettings = {
-  livereload: true,
-  open: true,
+	livereload: true,
+	open: true,
 };
 
 gulp.task("html:docs", function () {
-  return gulp
-    .src(["./src/html/**/*.html", "!./src/html/blocks/*.html"])
-    .pipe(changed("./docs/"))
-    .pipe(plumber(plumberNotify("Html")))
-    .pipe(fileInclude(fileIncludeSettings))
-    .pipe(
-      replace(
-        /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
-        "$1./$4$5$7$1"
-      )
-    )
-    .pipe(
-      typograf({
-        locale: ["ru", "en-US"],
-        htmlEntity: { type: "digit" },
-        safeTags: [
-          ["<\\?php", "\\?>"],
-          ["<no-typography>", "</no-typography>"],
-        ],
-      })
-    )
-
-    .pipe(
-      webpHTML({
-        extensions: ["jpg", "jpeg", "png", "gif", "webp"],
-        retina: {
-          1: "",
-          2: "@2x",
-        },
-      })
-    )
-    .pipe(htmlclean())
-    .pipe(gulp.dest("./docs/"));
+	return gulp
+		.src(["./src/html/**/*.html", "./src/html/blocks/*.html"])
+		.pipe(changed("./docs/"))
+		.pipe(plumber(plumberNotify("Html")))
+		.pipe(fileInclude(fileIncludeSettings))
+		.pipe(
+			replace(
+				/(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+				"$1./$4$5$7$1"
+			)
+		)
+		.pipe(
+			typograf({
+				locale: ["ru", "en-US"],
+				htmlEntity: { type: "digit" },
+				safeTags: [
+					["<\\?php", "\\?>"],
+					["<no-typography>", "</no-typography>"],
+				],
+			})
+		)
+		.pipe(htmlclean())
+		.pipe(gulp.dest("./docs/"));
 });
 
 gulp.task("sass:docs", function () {
-  return gulp
-    .src("./src/scss/*.scss")
-    .pipe(changed("./docs/css/"))
-    .pipe(plumber(plumberNotify("Styles")))
-    .pipe(sourceMaps.init())
-    .pipe(autoprefixer())
-    .pipe(sassGlob())
-    .pipe(webpcss())
-    .pipe(groupMedia())
-    .pipe(sass())
-    .pipe(
-      replace(
-        /(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
-        "$1$2$3$4$6$1"
-      )
-    )
-    .pipe(csso())
-    .pipe(sourceMaps.write())
-    .pipe(gulp.dest("./docs/css/"));
+	return (
+		gulp
+			.src("./src/scss/*.scss")
+			.pipe(changed("./docs/css/"))
+			.pipe(plumber(plumberNotify("Styles")))
+			.pipe(sourceMaps.init())
+			.pipe(autoprefixer())
+			.pipe(sassGlob())
+			// .pipe(webpcss())
+			.pipe(groupMedia())
+			.pipe(sass())
+			.pipe(
+				replace(
+					/(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+					"$1$2$3$4$6$1"
+				)
+			)
+			.pipe(csso())
+			.pipe(sourceMaps.write())
+			.pipe(gulp.dest("./docs/css/"))
+	);
 });
 
 gulp.task("js:docs", () => {
-  return gulp
-    .src("./src/js/*.js")
-    .pipe(changed("./docs/js"))
-    .pipe(plumber(plumberNotify("JS")))
-    .pipe(babel())
-    .pipe(webpack(require("../webpack.config.js")))
-    .pipe(gulp.dest("./docs/js"));
+	return gulp
+		.src("./src/js/*.js")
+		.pipe(changed("./docs/js"))
+		.pipe(plumber(plumberNotify("JS")))
+		.pipe(babel())
+		.pipe(webpack(require("../webpack.config.js")))
+		.pipe(gulp.dest("./docs/js"));
 });
 
 gulp.task("images:docs", function () {
-  return gulp
-    .src("./src/img/**/*")
-    .pipe(changed("./docs/img/"))
-    .pipe(
-      imagemin([
-        imageminWebp({
-          quality: 85,
-        }),
-      ])
-    )
-    .pipe(extReplace(".webp"))
-    .pipe(gulp.dest("./docs/img/"))
-    .pipe(gulp.src("./src/img/**/*"))
-    .pipe(changed("./docs/img/"))
-    .pipe(
-      imagemin(
-        [
-          imagemin.gifsicle({ interlaced: true }),
-          imagemin.mozjpeg({ quality: 85, progressive: true }),
-          imagemin.optipng({ optimizationLevel: 5 }),
-        ],
-        { verbose: true }
-      )
-    )
-    .pipe(gulp.dest("./docs/img/"));
+	return gulp
+		.src("./src/img/**/*.*")
+		.pipe(changed("./docs/img/"))
+		.pipe(
+			imagemin([
+				imageminWebp({
+					quality: 85,
+				}),
+			])
+		)
+		.pipe(gulp.dest("./docs/img/"))
+		.pipe(gulp.src("./src/img/**/*"))
+		.pipe(changed("./docs/img/"))
+		.pipe(
+			imagemin(
+				[
+					imagemin.gifsicle({ interlaced: true }),
+					imagemin.mozjpeg({ quality: 85, progressive: true }),
+					imagemin.optipng({ optimizationLevel: 5 }),
+				],
+				{ verbose: true }
+			)
+		)
+		.pipe(gulp.dest("./docs/img/"));
 });
 
 gulp.task("files:docs", function () {
-  return gulp
-    .src("./src/files/**/*")
-    .pipe(changed("./docs/files/"))
-    .pipe(gulp.dest("./docs/files/"));
+	return gulp
+		.src("./src/files/**/*")
+		.pipe(changed("./docs/files/"))
+		.pipe(gulp.dest("./docs/files/"));
 });
 
 gulp.task("server:docs", function () {
-  return gulp.src("./docs").pipe(server(fileStartServerSettings));
+	return gulp.src("./docs").pipe(server(fileStartServerSettings));
 });
 
 gulp.task("clean:docs", function (done) {
-  if (fs.existsSync("./docs/"))
-    return gulp.src("./docs/", { read: false }).pipe(clean({ force: true }));
-  done();
+	if (fs.existsSync("./docs/"))
+		return gulp.src("./docs/", { read: false }).pipe(clean({ force: true }));
+	done();
 });
